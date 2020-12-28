36fa71666bd885d971551891bf95b98b1699fdd6
shangqi <shangqi.lai@monash.edu>
2020-12-28 18:07:46 +0800

    Clean the testing code

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 int main() {
     SSEClient client;
 
-    cout << duration_cast<microseconds>(system_clock::now().time_since_epoch()).count() << endl;
+//    cout << duration_cast<microseconds>(system_clock::now().time_since_epoch()).count() << endl;
     for (int i = 0; i < 200000; ++i) {
         client.update(INS, "test", i);
     }
-    cout << duration_cast<microseconds>(system_clock::now().time_since_epoch()).count() << endl;
+//    cout << duration_cast<microseconds>(system_clock::now().time_since_epoch()).count() << endl;
     for (int i = 0; i < 10000; ++i) {
         client.update(DEL, "test", i);
     }
 
     vector<int> results = client.search("test");
-//    for (int res : results) {
-//        cout << res << endl;
-//    }
+    for (int res : results) {
+        cout << res << endl;
+    }
     return 0;
 }
